307b9700dd7e0d733c7a3a53452c8c0d8712674e
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2020-10-22 12:28:29 +0200

    feat(server): better error handling

--- server/src/services/testssl-jobs/testssl-jobs.class.js
+++ server/src/services/testssl-jobs/testssl-jobs.class.js
@@ -9,6 +9,51 @@ function buildResult (item) {
   }
 }
 
+function buildError (code, message) {
+  switch (code) {
+    case 400:
+      return new errors.BadRequest(message)
+    case 401:
+      return new errors.NotAuthenticated(message)
+    case 402:
+      return new errors.PaymentError(message)
+    case 403:
+      return new errors.Forbidden(message)
+    case 404:
+      return new errors.NotFound(message)
+    case 405:
+      return new errors.MethodNotAllowed(message)
+    case 406:
+      return new errors.NotAcceptable(message)
+    case 408:
+      return new errors.Timeout(message)
+    case 409:
+      return new errors.Conflict(message)
+    case 410:
+      return new errors.Gone(message)
+    case 411:
+      return new errors.LengthRequired(message)
+    case 422:
+      return new errors.Unprocessable(message)
+    case 429:
+      return new errors.TooManyRequests(message)
+    case 500:
+      return new errors.GeneralError(message)
+    case 501:
+      return new errors.NotImplemented(message)
+    case 502:
+      return new errors.BadGateway(message)
+    case 503:
+      return new errors.Unavailable(message)
+    default:
+      return new errors.GeneralError(message)
+  }
+}
+
+function buildKubernetesError (error) {
+  return buildError(error.error.code, error.error.message)
+}
+
 exports.TestsslJobs = class TestsslJobs {
   constructor (app) {
     this.kubeConfig = app.get('kubeConfig')
@@ -26,7 +71,7 @@ exports.TestsslJobs = class TestsslJobs {
 
       return response.items.map(item => buildResult(item))
     } catch (error) {
-      throw new errors.GeneralError(error)
+      throw buildKubernetesError(error)
     }
   }
 
@@ -41,7 +86,7 @@ exports.TestsslJobs = class TestsslJobs {
 
       return buildResult(response)
     } catch (error) {
-      throw new errors.GeneralError(error)
+      throw buildKubernetesError(error)
     }
   }
 
@@ -79,7 +124,7 @@ exports.TestsslJobs = class TestsslJobs {
 
       return buildResult(response)
     } catch (error) {
-      throw new errors.GeneralError(error)
+      throw buildKubernetesError(error)
     }
   }
 }
