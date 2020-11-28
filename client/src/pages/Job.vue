17639bd46051907ede8673f6251090eaae8e9bdb
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2020-11-28 16:33:40 +0100

    feat(client): display job output

--- client/src/pages/Job.vue
+++ client/src/pages/Job.vue
@@ -1,8 +1,5 @@
 <template>
   <q-page class="q-pa-md">
-    <h4 class="q-mt-none gt-sm">
-      {{ $t('menu_testssl') }}
-    </h4>
     <q-card v-if="testsslJob" bordered class="bg-blue-2" flat>
       <q-card-section>
         <div>Id: {{ testsslJob.id }}</div>
@@ -11,6 +8,7 @@
           <div>Value: {{ parameter.value }}</div>
         </div>
         <div>Phase: {{ testsslJob.phase }}</div>
+        <pre class="bg-black overflow-auto q-ma-none q-pa-md rounded-borders text-white" v-html="html" />
       </q-card-section>
     </q-card>
     <q-card v-if="testsslJobError" bordered class="bg-red-2 text-negative" flat>
@@ -24,6 +22,7 @@
 </template>
 
 <script>
+import AnsiToHtml from 'ansi-to-html'
 import { makeGetMixin } from 'feathers-vuex'
 
 export default {
@@ -31,6 +30,25 @@ export default {
   mixins: [makeGetMixin({
     service: 'testssl-jobs',
     id () { return this.$route.params.id }
-  })]
+  })],
+  computed: {
+    artifact: function () {
+      return function (type) {
+        const artifactId = `${this.testsslJob.id}/output.${type}`
+        const artifact = this.testsslJob.artifacts.find(artifact => artifact.id === artifactId)
+
+        return artifact
+      }
+    },
+    html: function () {
+      const log = this.artifact('log').content
+      const html = new AnsiToHtml({ newline: true }).toHtml(log)
+
+      return html
+    },
+    testsslJobParams () {
+      return { $populateParams: { name: 'withArtifacts' } }
+    }
+  }
 }
 </script>
