322bb9063bf0b2fda202737ec18d6b1000aa02b4
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2020-12-04 12:11:32 +0100

    feat(client): work on job list

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <div class="bg-white q-pa-md rounded-borders">
+    <div class="q-pa-md rounded-borders bg-white">
       <q-form class="q-gutter-md" greedy ref="form" @submit="submit">
         <q-input :label="$t('target')" lazy-rules outlined :rules="targetRules" stack-label v-model="target" />
         <div class="row justify-end">
@@ -13,28 +13,45 @@
         </div>
       </q-form>
     </div>
-    <div v-for="testsslJob in testsslJobs" :key="testsslJob.id">
-      <q-card v-if="testsslJob" bordered class="bg-blue-2 cursor-pointer" flat @click="$router.push(`/${testsslJob.id}`)">
-        <q-card-section>
-          <div>Id: {{ testsslJob.id }}</div>
-          <div v-for="parameter in testsslJob.parameters" :key="parameter.name">
-            <div>Name: {{ parameter.name }}</div>
-            <div>Value: {{ parameter.value }}</div>
+    <div class="q-mt-md">
+      <div class="row q-col-gutter-md">
+        <div v-for="testsslJob in testsslJobs" :key="testsslJob.id" class="col-xs-12 col-sm-6" >
+          <div class="row cursor-pointer rounded-borders bg-white" @click="$router.push(`/${testsslJob.id}`)">
+            <div class="text-weight-bold col-xs-12 col-md-6 q-pa-sm text-uppercase ellipsis">
+              {{ parameter(testsslJob, 'target') }}
+            </div>
+            <div class="column items-center justify-center col-xs-6 col-md-3 q-pa-sm">
+              <div class="text-center">
+                {{ $t('overall_grade') }}
+              </div>
+              <div class="q-mt-sm">
+                <q-avatar v-if="finding(testsslJob, 'overall_grade')" :color="gradeColor(finding(testsslJob, 'overall_grade'))" rounded size="64px" text-color="white">
+                  {{ finding(testsslJob, 'overall_grade') }}
+                </q-avatar>
+                <q-skeleton v-else animation="fade" size="64px" type="rect" />
+              </div>
+            </div>
+            <div class="column items-center justify-center col-xs-6 col-md-3 q-pa-sm">
+              <div class="text-center">
+                {{ $t('final_score') }}
+              </div>
+              <div class="q-mt-sm">
+                <q-avatar v-if="finding(testsslJob, 'final_score')" :color="scoreColor(finding(testsslJob, 'final_score'))" rounded size="64px" text-color="white">
+                  {{ finding(testsslJob, 'final_score') }}
+                </q-avatar>
+                <q-skeleton v-else animation="fade" size="64px" type="rect" />
+              </div>
+            </div>
           </div>
-          <div>
-            <q-spinner v-if="testsslJob.phase === 'Running'" class="on-left" :color="jobColor(testsslJob)" size="sm" />
-            <q-icon v-if="testsslJob.phase !== 'Running'" class="on-left" :color="jobColor(testsslJob)" :name="jobIcon(testsslJob)" size="sm" />
-            {{ testsslJob.phase }}
-          </div>
-        </q-card-section>
-      </q-card>
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
 import { makeFindMixin } from 'feathers-vuex'
-import { jobColorHelper, jobIconHelper } from '../helpers/job'
+import { jobFindingHelper, jobGradeColorHelper, jobParameterHelper, jobScoreColorHelper } from '../helpers/job'
 import { validateHelper } from '../helpers/validate'
 import Joi from '@hapi/joi'
 
@@ -50,20 +67,20 @@ export default {
     }
   },
   computed: {
-    jobColor: function () {
-      return function (job) {
-        return jobColorHelper(job)
-      }
+    finding: function () {
+      return jobFindingHelper
     },
-    jobIcon: function () {
-      return function (job) {
-        return jobIconHelper(job)
-      }
+    gradeColor () {
+      return jobGradeColorHelper
+    },
+    parameter () {
+      return jobParameterHelper
+    },
+    scoreColor () {
+      return jobScoreColorHelper
     },
     testsslJobsParams () {
-      const query = {}
-
-      return { query }
+      return { $populateParams: { name: 'withArtifacts' } }
     },
     targetRules: function () {
       return validateHelper(Joi.string().domain())
